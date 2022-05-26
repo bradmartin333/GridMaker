6c4fd1d050f3e27af56713f143df0a25e9035a41
BradDesktop <brad.martin@xdisplay.com>
2022-05-26 12:26:58 -0400

    create proper class lib

---
+++ GridMaker/GridMaker/Composer.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace GridMaker
+{
+    public partial class Composer : Form
+    {
+        private readonly string GridPath = $"{Path.GetTempPath()}GridMaker.xml";
+        public static Grid Grid = new Grid();
+
+        /// <summary>
+        /// Open a Grid Maker Composer window
+        /// and load the last used Grid from the
+        /// temp directory
+        /// </summary>
+        public Composer()
+        {
+            InitializeComponent();
+            if (File.Exists(GridPath)) LoadGrid();
+        }
+
+        private void BtnValidateAndSave_Click(object sender, EventArgs e)
+        {
+            SaveGrid();
+        }
+
+        private bool ValidateGrid()
+        {
+            Grid = new Grid()
+            {
+                Name = TxtName.Text,
+                StepA = new Step(new Size((int)NumStepACountX.Value, (int)NumStepACountY.Value), new PointF((float)NumStepAPitchX.Value, (float)NumStepAPitchY.Value)),
+                StepB = new Step(new Size((int)NumStepBCountX.Value, (int)NumStepBCountY.Value), new PointF((float)NumStepBPitchX.Value, (float)NumStepBPitchY.Value)),
+                StepC = new Step(new Size((int)NumStepCCountX.Value, (int)NumStepCCountY.Value), new PointF((float)NumStepCPitchX.Value, (float)NumStepCPitchY.Value)),
+            };
+            bool validA = MakeSkippedIndices(Grid.StepA, RTBA.Text);
+            bool validB = MakeSkippedIndices(Grid.StepB, RTBB.Text);
+            bool validC = MakeSkippedIndices(Grid.StepC, RTBC.Text);
+            return validA && validB && validC;
+        }
+
+        private void RepopulateUI()
+        {
+            TxtName.Text = Grid.Name;
+            NumStepACountX.Value = Grid.StepA.Array.Width;
+            NumStepACountY.Value = Grid.StepA.Array.Height;
+            NumStepBCountX.Value = Grid.StepB.Array.Width;
+            NumStepBCountY.Value = Grid.StepB.Array.Height;
+            NumStepCCountX.Value = Grid.StepC.Array.Width;
+            NumStepCCountY.Value = Grid.StepC.Array.Height;
+            NumStepAPitchX.Value = (decimal)Grid.StepA.Pitch.X;
+            NumStepAPitchY.Value = (decimal)Grid.StepA.Pitch.Y;
+            NumStepBPitchX.Value = (decimal)Grid.StepB.Pitch.X;
+            NumStepBPitchY.Value = (decimal)Grid.StepB.Pitch.Y;
+            NumStepCPitchX.Value = (decimal)Grid.StepC.Pitch.X;
+            NumStepCPitchY.Value = (decimal)Grid.StepC.Pitch.Y;
+            PopulateRTB(Grid.StepA, RTBA);
+            PopulateRTB(Grid.StepB, RTBB);
+            PopulateRTB(Grid.StepC, RTBC);
+        }
+
+        private void PopulateRTB(Step step, RichTextBox rtb)
+        {
+            rtb.Text = "";
+            foreach (Point point in step.SkippedIndices)
+                rtb.Text += $"{point.X}, {point.Y}\n";
+        }
+
+        private bool MakeSkippedIndices(Step step, string text)
+        {
+            string[] rows = text.Split('\n');
+            List<Point> points = new List<Point>();
+            foreach (string row in rows)
+            {
+                string[] cols = row.Split(',');
+                if (cols.Length == 2)
+                {
+                    bool validX = int.TryParse(cols[0].Replace(" ", ""), out int x);
+                    bool validY = int.TryParse(cols[1].Replace(" ", ""), out int y);
+                    if (validX && validY) points.Add(new Point(x, y));
+                }
+            }
+            step.SkippedIndices = points;
+            return true;
+        }
+
+        private void SaveGrid(string path = null)
+        {
+            if (ValidateGrid())
+            {
+                using (StreamWriter stream = new StreamWriter(string.IsNullOrEmpty(path) ? GridPath : path))
+                {
+                    XmlSerializer x = new XmlSerializer(typeof(Grid));
+                    x.Serialize(stream, Grid);
+                }
+                RepopulateUI();
+            } 
+        }
+
+        private void LoadGrid(string path = null)
+        {
+            try
+            {
+                using (StreamReader stream = new StreamReader(string.IsNullOrEmpty(path) ? GridPath : path))
+                {
+                    XmlSerializer x = new XmlSerializer(typeof(Grid));
+                    Grid = (Grid)x.Deserialize(stream);
+                }
+                RepopulateUI();
+            }
+            catch (Exception) { }
+        }
+
+        private void NewToolStripButton_Click(object sender, EventArgs e)
+        {
+            Grid = new Grid();
+            RepopulateUI();
+        }
+
+        private void OpenToolStripButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.RestoreDirectory = true;
+                ofd.Title = "Open Grid Maker";
+                ofd.Filter = "XML file(*.xml)| *.xml";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    LoadGrid(ofd.FileName);
+            }
+        }
+
+        private void SaveToolStripButton_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "XML file(*.xml)| *.xml";
+                sfd.Title = "Save Grid Maker";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(sfd.FileName)) File.Delete(sfd.FileName);
+                    SaveGrid(sfd.FileName);
+                }
+            }
+        }
+
+        private void HelpToolStripButton_Click(object sender, EventArgs e)
+        {
+            string helpStr = "Step A should have the largest pitches and Step C should have the smallest pitches.\n" +
+                             "The Steps are nested to allow for greater customization.\n" +
+                             "Enter skipped indices within the array as a zero-indexed CSV and a new line between each entry.\n" +
+                             "\tEx: 0, 1";
+            MessageBox.Show(helpStr, "Grid Maker Help");
+        }
+
+        private void BtnDone_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+    }
+}
---
+++ GridMaker/GridMaker/Generator.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace GridMaker
+{
+    public class Generator
+    {
+        private readonly PointF SW, SE;
+        private PointF CenterOfRotation;
+
+        /// <summary>
+        /// Storage for Grid points
+        /// </summary>
+        public struct Node
+        {
+            public PointF Location;
+            public Point A;
+            public Point B;
+            public Point C;
+            /// <summary>
+            /// Get TSV string representation of the Node
+            /// </summary>
+            /// <returns>
+            /// One-indexed node location string
+            /// </returns>
+            public override string ToString() => $"{A.Y + 1}\t{A.X + 1}\t{B.Y + 1}\t{B.X + 1}\t{C.Y + 1}\t{C.X + 1}\t";
+        }
+
+        /// <summary>
+        /// Initialize the Grid Generator
+        /// </summary>
+        /// <param name="sw">
+        /// Stage coordinates in mm of SW corner of ROI
+        /// </param>
+        /// <param name="se">
+        /// Stage coordinates in mmof SE corner of ROI
+        /// </param>
+        public Generator(PointF sw, PointF se)
+        {
+            SW = sw;
+            SE = se;
+            UpdateCenterOfRotation();
+        }
+
+        /// <summary>
+        /// Get Node position rotated to ROI
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns>
+        /// Stage coordinates of aligned node in mm
+        /// </returns>
+        public PointF GetStagePosition(Node node)
+        {
+            PointF globalPos = new PointF(SW.X - node.Location.X, SW.Y - node.Location.Y);
+            RotatePoint(ref globalPos);
+            return globalPos;
+        }
+
+        private void RotatePoint(ref PointF point, bool getCOR = false)
+        {
+            if (point == SW && !getCOR)
+                return;
+            double angle = -1 * Math.Atan((SE.Y - SW.Y) / Math.Abs(SE.X - SW.X));
+            float x = (float)(point.X * Math.Cos(angle) - point.Y * Math.Sin(angle));
+            float y = (float)(point.X * Math.Sin(angle) + point.Y * Math.Cos(angle));
+            point = new PointF(x + CenterOfRotation.X, y + CenterOfRotation.Y);
+        }
+
+        private void UpdateCenterOfRotation()
+        {
+            CenterOfRotation = new PointF(0, 0);
+            PointF SWcopy = SW;
+            RotatePoint(ref SWcopy, true);
+            CenterOfRotation = new PointF(SW.X - SWcopy.X, SW.Y - SWcopy.Y);
+        }
+
+        /// <summary>
+        /// Generate the Grid node
+        /// </summary>
+        /// <returns>
+        /// List of nodes that make up the grid in a serpentine order
+        /// </returns>
+        public List<Node> Generate()
+        {
+            List<Node> points = new List<Node>();
+            for (int i = 0; i < Composer.Grid.StepA.Array.Width; i++)
+            {
+                for (int j = 0; j < Composer.Grid.StepA.Array.Height; j++)
+                {
+                    Point point = new Point(i, j);
+                    if (Composer.Grid.StepA.SkippedIndices.Contains(point)) continue;
+                    points.Add(new Node()
+                    {
+                        Location = new PointF(
+                            i * Composer.Grid.StepA.Pitch.X,
+                            j * Composer.Grid.StepA.Pitch.Y),
+                        A = point,
+                        B = Point.Empty,
+                        C = Point.Empty
+                    });
+                }
+            }
+
+            Node[] basePoints = (Node[])points.ToArray().Clone();
+            for (int k = 0; k < basePoints.Count(); k++)
+            {
+                for (int i = 0; i < Composer.Grid.StepB.Array.Width; i++)
+                {
+                    for (int j = 0; j < Composer.Grid.StepB.Array.Height; j++)
+                    {
+                        Point point = new Point(i, j);
+                        if (point == Point.Empty) continue;
+                        if (Composer.Grid.StepB.SkippedIndices.Contains(point)) continue;
+                        points.Add(new Node
+                        {
+                            Location = new PointF(
+                                basePoints[k].Location.X + i * Composer.Grid.StepB.Pitch.X,
+                                basePoints[k].Location.Y + j * Composer.Grid.StepB.Pitch.Y),
+                            A = basePoints[k].A,
+                            B = point,
+                            C = Point.Empty
+                        });
+                    }
+                }
+            }
+
+            basePoints = (Node[])points.ToArray().Clone();
+            for (int k = 0; k < basePoints.Count(); k++)
+            {
+                for (int i = 0; i < Composer.Grid.StepC.Array.Width; i++)
+                {
+                    for (int j = 0; j < Composer.Grid.StepC.Array.Height; j++)
+                    {
+                        Point point = new Point(i, j);
+                        if (point == Point.Empty) continue;
+                        if (Composer.Grid.StepC.SkippedIndices.Contains(point)) continue;
+                        points.Add(new Node
+                        {
+                            Location = new PointF(
+                                basePoints[k].Location.X + i * Composer.Grid.StepC.Pitch.X,
+                                basePoints[k].Location.Y + j * Composer.Grid.StepC.Pitch.Y),
+                            A = basePoints[k].A,
+                            B = basePoints[k].B,
+                            C = point
+                        });
+                    }
+                }
+            }
+
+            return Serpentize(points);
+        }
+
+        private List<Node> Serpentize(List<Node> points)
+        {
+            List<Node> output = new List<Node>();
+            var groups = points.OrderBy(x => x.Location.X).OrderBy(x => x.Location.Y).GroupBy(x => x.Location.X);
+            int idx = 0;
+            foreach (IGrouping<float, Node> group in groups)
+            {
+                if (idx % 2 != 0) // Odd groups
+                    output.AddRange(group.ToArray().Reverse());
+                else
+                    output.AddRange(group.ToArray());
+                idx++;
+            }
+            return output;
+        }
+    }
+}
